3313147291cf18d99984427560e278c536e7219f
lila <lila@DESKTOP-IGMFGCT>
2022-05-17 18:56:47 -0500

    Add project files.

---
+++ Practice01/Program1.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Lila Lim!");
+
+// 1 - Develop an algorithm that requests 2 numbers and calculates and prints the product.
+
+using System;
+
+namespace ReadLine1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            string sinput1, sinput2;
+            int input1, input2, sum;
+
+            Console.Write("\nInput the first number: ");
+            sinput1 = Console.ReadLine();
+            input1 = Convert.ToInt32(sinput1);
+            //Console.WriteLine(input1);
+
+            Console.Write("Input the second number: ");
+            sinput2 = Console.ReadLine();
+            input2 = Convert.ToInt32(sinput2);
+            //Console.WriteLine(input2);
+
+            sum = input1 + input2;
+
+            Console.WriteLine("\nThe sum of two inputs is " + sum);
+        }
+    }
+}
+            
